f514c3b24edd242592a000b7270b691b78e1295d
Eyong <eyong.ldg@live.com>
2022-03-25 11:03:23 +0800

    Screen Navigation

---
+++ navigator/lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'sub/home_screen.dart';
+void main(){
+  runApp(const MaterialApp(
+    home: Home(),
+  ));
+}
---
+++ navigator/lib/sub/screen2.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SecondRoute extends StatefulWidget {
+  const SecondRoute({Key? key}) : super(key: key);
+
+  @override
+  State<SecondRoute> createState() => _SecondRouteState();
+}
+
+class _SecondRouteState extends State<SecondRoute> {
+  @override
+  Widget build(BuildContext context) {
+    return const Text("Hello There");
+  }
+}
